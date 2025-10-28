2c16ec56554458c57a6d91cdb2231cd19c7b23e1
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-10-28 10:54:03 +0530

    navbar updation

---
+++ src/common/components/Navbar.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-black text-white h-12 px-5 flex justify-between items-center">
+    <div class="text-2xl font-medium">Color - Palette</div>
+    <div>button</div>
+  </div>
+</template>
